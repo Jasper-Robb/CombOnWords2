588848fe6d4a6b5959199df5263f9c392804024b
Jasper Robb <jasperdrobb@hotmail.com>
2024-01-29 19:37:48 -0600

    Changed definition of nonempty

--- CombOnWords2/Basic.lean
+++ CombOnWords2/Basic.lean
@@ -6,7 +6,7 @@ abbrev Word (α : Type*) [Fintype α] := FreeMonoid α
 variable {α : Type*} [Fintype α] 
 
 def Overlap (w : Word α) : Prop :=
-  ∃ (B : Word α), B ≠ 1 ∧ w = B * B * B.take 1
+  ∃ (B : Word α), 0 < |B| ∧ w = B * B * B.take 1
 
 theorem chapter1_question2 (u : Word α) (hu : Overlap u)
     : ∃ (v w z : Word α), u = w * v ∧ u = z * w ∧ |w| > |v| := by
@@ -19,28 +19,26 @@ theorem chapter1_question2 (u : Word α) (hu : Overlap u)
     simpa only [freemonoid_to_list, List.take_append_drop]
   apply And.intro
   · rwa [← mul_assoc]
-  · simpa [freemonoid_to_list] using Nat.sub_lt (List.length_pos.mpr hBl) Nat.one_pos
+  · simpa [freemonoid_to_list] using Nat.sub_lt hBl Nat.one_pos
 
 theorem chapter1_question3 (u : Word α) (hu : Overlap u)
-    : (∃ (x : Word α), x ≠ 1 ∧ u = x * x * x) ∨ 
-      (∃ (x y : Word α), x ≠ 1 ∧ y ≠ 1 ∧ u = x * y * x * y * x) := by
+    : (∃ (x : Word α), 0 < |x| ∧ u = x * x * x) ∨ 
+      (∃ (x y : Word α), 0 < |x| ∧ 0 < |y| ∧ u = x * y * x * y * x) := by
   rcases hu with ⟨B, hBr, hBl⟩
   cases eq_or_ne |B| 1 with
   | inl h =>
     apply Or.inl
     apply Exists.intro B
     apply And.intro
-    · exact List.length_pos.mp <| Nat.lt_of_sub_eq_succ h
+    · exact hBr
     · simpa [hBl] using List.take_length_le <| Nat.le_of_eq h
   | inr h =>
     apply Or.inr
     apply Exists.intro <| B.take 1
     apply Exists.intro <| B.drop 1
     apply And.intro
-    · apply List.length_pos.mp
-      simpa [freemonoid_to_list] using List.length_pos.mpr hBr
+    · simpa [freemonoid_to_list]
     apply And.intro
-    · apply List.length_pos.mp
-      simpa [freemonoid_to_list] using Nat.lt_of_le_of_ne (List.length_pos.mpr hBr) h.symm
+    · simpa [freemonoid_to_list] using Nat.lt_of_le_of_ne hBr h.symm
     · conv => rhs; lhs; rw [mul_assoc]
       simpa only [freemonoid_to_list, List.take_append_drop]
