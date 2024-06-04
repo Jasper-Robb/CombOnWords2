0645f2c10126375ada55e2f0b4e245d30e793386
Jasper Robb <jasperdrobb@hotmail.com>
2024-06-04 11:44:37 -0500

    Fix spacing in List.lean

--- CombOnWords2/List.lean
+++ CombOnWords2/List.lean
@@ -60,7 +60,6 @@ theorem reverse_take_one (l : List α) : (l.take 1).reverse = l.take 1 := by
     rfl
 
 
-
 theorem getLast_if_all (p : α → Prop) (l : List α) (hl : l ≠ []) : (∀ x ∈ l, p x) → p (l.getLast hl) :=
   (· (List.getLast l hl) (List.getLast_mem hl))
 
